d3bf62f2454419a6a2ea2fc16218bc4e0f6e8510
shahzeelhameed <bscs2012317@szabist.pk>
2025-05-06 02:33:02 +0500

    added rfq feature

--- controller/RFQ.js
+++ controller/RFQ.js
@@ -49,3 +49,30 @@ exports.createRFQ = async (req, res) => {
     return res.status(500).json({ message: 'Error creating RFQ', error });
   }
 };
+
+exports.getRFQsBySellerId = async (req, res) => {
+  try {
+    const { sellerId } = req.params;
+
+    console.log(sellerId);
+
+    if (!sellerId) {
+      return res
+        .status(400)
+        .json({ message: 'sellerId parameter is required' }); // 400 Bad Request
+    }
+
+    const rfqs = await RFQ.find({ seller_id: sellerId });
+
+    if (!rfqs || rfqs.length === 0) {
+      return res
+        .status(404)
+        .json({ message: `No RFQs found for sellerId: ${sellerId}` }); // 404 Not Found
+    }
+
+    return res.status(200).json(rfqs); // 200 OK
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Server error' }); // 500 Internal Server Error
+  }
+};
